db9a7bcb4d06643b24591b630b600cfbb825688e
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-06-15 19:20:50 +0530

    add

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "PromptifyMe",
+  description: "Discover & share AI Prompts",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
