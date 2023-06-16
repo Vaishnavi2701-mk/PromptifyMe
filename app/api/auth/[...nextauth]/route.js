a167310a1c5f82fe17c517a1624cab19fb2b3d77
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-06-17 00:21:28 +0530

    add

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 console.log({
   clientId: process.env.GOOGLE_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -14,7 +16,19 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if user already exists
+
+      // if not, create a new user
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
