29003c992ca756f18d3702aad930e0c583b8e390
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-06-17 14:57:06 +0530

    add

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,7 +7,44 @@ import { useRouter } from "next/router";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
