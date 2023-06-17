e68593e1f1f4e5217a5aa0539cdcc1cd94562d13
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-06-18 00:46:28 +0530

    add

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,18 +1,40 @@
-import { connectToDB } from "@utils/database";
+// import { connectToDB } from "@utils/database";
+// import Prompt from "@models/prompt";
+
+// export const GET = async (request) => {
+//   try {
+//     await connectToDB();
+
+//     const prompts = await Prompt.find({}).populate("creator");
+
+//     return new Response(JSON.stringify(prompts), {
+//       status: 200,
+//     });
+//   } catch (error) {
+//     return new Response("Failed to fetch all prompts", {
+//       status: 500,
+//     });
+//   }
+// };
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+import { headers } from "next/headers";
 
 export const GET = async (request) => {
+  const headersList = headers();
+  const referer = headersList.get("referer");
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
     return new Response(JSON.stringify(prompts), {
       status: 200,
+      headers: {
+        referer: referer,
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+      },
     });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", {
-      status: 500,
-    });
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
